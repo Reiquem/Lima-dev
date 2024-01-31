11f3079961b88f6a1f06e5cd7ad6579d188f98b6
Reiquem <dpls.diogo@outlook.com>
2024-01-31 13:24:01 -0300

    inicio criação das demais paginas 🚧

---
+++ pages/projects.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae impedit laudantium ut veritatis temporibus voluptatibus assumenda dolorem possimus necessitatibus eaque enim, at expedita maxime ad incidunt! Sapiente cumque nemo quia.</div>
+</template>
